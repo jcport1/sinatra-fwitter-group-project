21e45338448c311d739640c8c854454e698e2d1e
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-12-09 19:19:49 -0500

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
+
   has_secure_password
+   #this ActiveRecord method goes in the user model
+  #orivudes a reader/writer method for password
+  #provides a .authenticate method
   has_many :tweets
+
+  def slug
+    username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
 end
+  
